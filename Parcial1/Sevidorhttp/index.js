92e10985fc472b6993d05da7cc15f5ab3b47ae63
guadaluperosaldo <rosaldosandg@gmail.com>
2025-01-31 10:12:52 -0600

    Node

---
+++ Parcial1/Sevidorhttp/index.js
@@ -0,0 +1,22 @@
+/*var http = require('http');
+
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello Word!');
+})
+
+server.listen(3000, () => {
+    console.log("Servidor HTTP corriendo en pto 3000");
+});*/
+
+
+var http = require('http');
+
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
+    res.end('Hello Word!');
+})
+
+server.listen(3000, () => {
+    console.log("Servidor HTTP corriendo en pto 3000");
+});
