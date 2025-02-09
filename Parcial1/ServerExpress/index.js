642ab1d108a7b9053a25f49cc422c5603fd86930
guadaluperosaldo <rosaldosandg@gmail.com>
2025-02-08 18:50:03 -0600

    Ejercicio middleware

--- Parcial1/ServerExpress/index.js
+++ Parcial1/ServerExpress/index.js
@@ -1,15 +1,39 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const port=3000;
 
-app.get('/',(req,res,next)=>{
-    res.sendFile(__dirname+'/public/index.html');
+//Middleware incorporado en express
+app.use(express.json());
+app.use(express.text());
+
+app.use('/',(req,res,next)=>{
+    console.log("Petición al server");
+    next();
+});
+app.use('/',(req,res,next)=>{
+    console.log("2da función Middleware");
+    next();
 });
 
-app.post('/',(req,res)=>{
+app.get('/alumno', (req, res) => {
+    console.log(req.query);
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/sistemas/control', (req, res) => {
+    console.log(req.body);
     res.send('Hello World!');
 });
 
-app.listen(port,()=>{
-    console.log(`Server running at http://localhost:${port}`);
-})
+app.patch('/maestros/:control', (req, res) => {
+    console.log(req.body);
+    res.send('Hello World!');
+});
+
+app.use((req, res) => {
+    res.status(404).send("Error 404");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor express escuchando en http://localhost:${port}`);
+});
