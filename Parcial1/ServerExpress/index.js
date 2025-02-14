2620f07a1f9a202fef4e6bd0a27b9ecfa3f169c4
guadaluperosaldo <rosaldosandg@gmail.com>
2025-02-13 21:57:54 -0600

    xml / form

--- Parcial1/ServerExpress/index.js
+++ Parcial1/ServerExpress/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const xmlparser = require('express-xml-bodyparser');
+const multer = require('multer');
+const path = require('path');
 const app = express();
 const port=3000;
+
+const folder = path.join(__dirname+'/artchivosrec/');
+const upload = multer({dest:folder});
 
 //Middleware incorporado en express
 app.use(express.json());
 app.use(express.text());
+app.use(xmlparser());
+app.use(upload.single('archivo'));
 
 app.use('/',(req,res,next)=>{
     console.log("Petición al server");
@@ -28,6 +36,15 @@ app.post('/sistemas/control', (req, res) => {
 app.patch('/maestros/:control', (req, res) => {
     console.log(req.body);
     res.send('Hello World!');
+});
+
+app.post('/prefectos', (req, res) => {
+    //console.log(req.body);
+    //res.send('Hello World!');
+    console.log(`Se recibio el archivo: ${req.file.originalname}`); 
+    console.log(req.body); 
+    console.log('Se recibio el formulario:'+JSON.stringify(req.body)); 
+    res.json(req.body); 
 });
 
 app.use((req, res) => {
